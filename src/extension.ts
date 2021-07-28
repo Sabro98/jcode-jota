10da85088e0250365bcc527b5668a79cf39d5067
Sabro98 <inhyuk04@gmail.com>
2021-07-28 17:23:39 +0900

    update: make functional

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,131 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { ExtensionContext } from 'vscode';
+import { ExtensionContext, workspace, window } from 'vscode';
+import fetch, { Response } from 'node-fetch';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+async function getProblemCode(): Promise<String | undefined> {
+  const problemCode = await window.showInputBox({
+    placeHolder: 'Write problem code',
+  });
+
+  return problemCode;
+}
+
+function getTextFromEditor(): String | undefined {
+  const editor = window.activeTextEditor;
+  if (editor) {
+    const document = editor.document;
+    let documentText = document.getText();
+    documentText = documentText.replace(/\r?\n/g, '\r\n');
+
+    return documentText;
+  }
+}
+
+async function submitCode(
+  userId: String,
+  problemCode: String,
+  sourceCode: String
+) {
+  const URL = 'http://203.254.143.156:8001/api/v2/submit/jcode';
+  const data = {
+    judge_id: 'jota-judge',
+    language: 'C',
+    // jcode의 사용자 id를 사용해야하나? Jcode <-> Jota 계정 과의 연관성 확인 필요
+    user: 'admin',
+    problem: problemCode,
+    source: sourceCode,
+  };
+
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-type': 'application/json',
+    },
+  };
+
+  //send rest to URL
+  fetch(URL, options)
+    .then((response) => response.json())
+    .then((post) => {
+      //show result to user
+      const results: string[] = showResult(post);
+
+      //save result to ./result
+      saveAsFile(results);
+    })
+    .catch((error) => vscode.window.showErrorMessage(`Error: ${error}`));
+}
+
+function showResult(post: string): string[] {
+  const submitResultArrs: string[] = [];
+  const resultEmoj: string[] = [];
+
+  //process result for show
+  JSON.parse(post).forEach((element: string[], index: number) => {
+    const result = element[0] == 'AC' ? '✅' : '❌';
+    const spendTime = element[1];
+    const spendMemory = element[2];
+    const resultMsg = `testCase #${index} → ${result} time: ${spendTime}s | memory: ${spendMemory} KB`;
+
+    submitResultArrs.push(resultMsg);
+    resultEmoj.push(` case #${index + 1} : ${result} `);
+  });
+
+  //show result use information message
+  window.showInformationMessage(resultEmoj.join(' '));
+
+  return submitResultArrs;
+}
+
+//save as text file
+async function saveAsFile(results: string[]): Promise<void> {
+  const path = require('path');
+  const fs = require('fs');
+  const saveFolderName = 'result';
+  const workSpaceFolders: readonly vscode.WorkspaceFolder[] | undefined =
+    workspace.workspaceFolders;
+
+  if (!workSpaceFolders) return;
+
+  const folderUri = workSpaceFolders[0].uri;
+  const basePath = path.join(folderUri.path, saveFolderName);
+
+  //폴더가 없다면 생성
+  !fs.existsSync(basePath) && fs.mkdirSync(basePath);
+
+  const fileUri = folderUri.with({
+    path: path.join(basePath, `${Date.now().toString()}.txt`),
+  });
+
+  const data = results.join('\n');
+
+  await workspace.fs.writeFile(fileUri, Buffer.from(data, 'utf8'));
+}
+
+//get User's id
+function getUserId(): string {
+  return 'admin';
+}
+
 export function activate(context: ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "jcode-jota" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    'jcode-jota.helloWorld',
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World from jcode-jota!');
-    }
-  );
+  const command = 'jcode-jota.submitCode';
 
-  const a: String = 'abc';
+  const disposable = vscode.commands.registerCommand(command, () => {
+    getProblemCode().then((problemCode) => {
+      if (!problemCode) return;
+      const sourceCode = getTextFromEditor();
+      if (!sourceCode) return;
+      const userId = getUserId();
+      if (!userId) return;
+
+      submitCode(userId, problemCode, sourceCode);
+    });
+  });
 
   context.subscriptions.push(disposable);
 }
