b908dfe513cf3f5001511fc230296ae270cb7bf8
Sabro98 <inhyuk04@gmail.com>
2021-11-18 10:58:44 +0900

    fix test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,21 @@
-import * as assert from 'assert';
+// import * as assert from 'assert';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
+// import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
+import { expect } from 'chai';
 
-suite('Extension Test Suite', () => {
-  vscode.window.showInformationMessage('Start all tests.');
-
-  test('Sample test', () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+describe('run test', () => {
+  it('test index', () => {
+    expect([1, 2, 3].indexOf(5)).to.equal(-1);
+    expect([1, 2, 3].indexOf(0)).to.equal(-1);
+    expect([1, 2, 3].indexOf(3)).to.equal(2);
   });
 });
+// suite('Extension Test Suite', () => {
+//   test('Sample test', () => {
+//     assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+//     assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+//   });
+// });
